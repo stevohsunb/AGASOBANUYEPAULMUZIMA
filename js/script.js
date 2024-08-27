bbd421a787a042713a7c8110dc55f7812068fe28
stevohsunb <153365365+stevohsunb@users.noreply.github.com>
2024-08-27 12:33:03 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// JavaScript to handle button click actions
+function ctaAction(action) {
+    switch(action) {
+        case 'like':
+            alert("Thank you for liking the video!");
+            break;
+        case 'comment':
+            alert("Feel free to leave a comment!");
+            break;
+        case 'subscribe':
+            alert("Thanks for subscribing! Stay tuned for more content.");
+            break;
+        case 'share':
+            alert("Share this video with your friends!");
+            break;
+    }
+}
